0aa28bad02025647dba6d30079386d0507b7002b
Kevin Kelm <devfecta@gmail.com>
2020-09-20 21:53:21 -0500

    Working on cached items.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './css/index.css';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { unregister } from './serviceWorker';
 
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js'
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+unregister();
 
 ReactDOM.render(
     <App />,
   document.getElementById('bodyTag')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
