1b2d7b43199e1cfcf45a675f91b653d53d3716d0
Kevin Kelm <devfecta@gmail.com>
2020-11-13 17:10:25 -0600

    Add pronunciation

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c538c574ca3fedd4d0ee666e1c2a8d16.js"
+  "/precache-manifest.d613a5719e2bfb4b4d80f567b6921167.js"
 );
 
 self.addEventListener('message', (event) => {
