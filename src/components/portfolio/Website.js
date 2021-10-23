88b5171d68d3ace690bfb1505c40a5cf54927021
DevFecta <devfecta@gmail.com>
2021-10-22 19:04:39 -0500

    Made a quick change.

--- src/components/portfolio/Website.js
+++ src/components/portfolio/Website.js
@@ -31,7 +31,7 @@ class Website extends Component {
             <article className={"portfolio-article p-5 " + classes}>
                 <picture>
                     <source srcSet={(this.props.url) ? "//image.thum.io/get/" + this.props.url : ""} media="(min-width: 1080px)" type="image/png" />
-                    <img src="../images/DevFecta.png" className="col-md-3 float-end m-3 img-fluid" width="auto" height="auto" alt={this.props.title} loading="lazy" style={{"boxShadow": "2px 2px 5px rgba(0, 0, 0, 0.5)"}} />
+                    <img src="./images/Devfecta.png" className="portfolio-img col-md-3 float-end m-3 img-fluid" width="auto" height="auto" alt={this.props.title} loading="lazy" style={{"boxShadow": "2px 2px 5px rgba(0, 0, 0, 0.5)"}} />
                 </picture>
                 
                 <h2>{this.props.title}</h2>
